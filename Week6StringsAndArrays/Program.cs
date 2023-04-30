649ed6923adc5a91af4a711041378835f6efecc0
Krete <krete121@gmail.com>
2023-04-30 12:45:20 +0300

    add Week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+string hello = "we are the champions?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for(int i = 0; i<hello.Length; i++)
+//{
+// if (char.IsWhiteSpace(hello[i]))
+// {
+//   WordCounter++;
+// }
+//}
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
+
+
+
+
+
+//int wordCounter = 0;
+//for (int i = 0; i < hello.Length; i++)
+{
+
+}
+
+//Console.WriteLine(hello.Length);
+
